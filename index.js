4321ae0f0d92483ad632dff42040afc3d4e06231
jayaanthsr <jayaanth80@gmail.com>
2024-12-28 11:42:16 +0530

    basic javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+obj={}
+obj["firstname"]="mah"
+obj["secondname"]="ashwin"
+obj["name"]="V"
+
+console.log(obj)
+
+a=10
+if(a%2==0){
+    console.log(a," is even");
+}
+else{
+    console.log(a," is odd");
+}
+
+a=10
+b=20
+
+console.log(a!=20)
+
